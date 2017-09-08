343ff460a572c076c882eb48e2a682327ae7f7ca
karlwithak <nhrynuik@gmail.com>
2017-09-07 21:48:47 -0400

    Added another option. Use hsl colors

--- static/translucent_circles/translucent_circles.rb
+++ static/translucent_circles/translucent_circles.rb
@@ -2,40 +2,46 @@ require 'victor'
 
 width = 1000
 height = 1000
-svg = Victor::SVG.new width: width, height: height, style: { background: '#fff' }
+circle_max_width = 300
+svg = Victor::SVG.new width: width, height: height, style: { background: 'hsl(230, 50%, 20%)' }
 
 class ColorDict
+    attr_accessor :opacity
     def initialize()
-        @red = 0
-        @green = 0
-        @blue = 0
-        @opacity = 0
+        @hue = 0
+        @saturation = "60%"
+        @lightness = "50%"
+        @alpha = "50%"
     end
 
     def next_color() 
-      @red = rand(256)
-      @green = rand(256)
-      @blue = rand(256)
-      @opacity = rand() / 1.3
+      @hue = rand(200..260)
     end
 
-    def to_s()
-      return "rgba(#{@red}, #{@green}, #{@blue}, #{@opacity})"
+    def to_hsla()
+      return "hsl(#{@hue}, #{@saturation}, #{@lightness})"
     end
 
-    def darker_color()
-      @red = [255, @red - 20].min
-      @green = [255, @green - 20].min
-      @blue = [255, @blue - 20].min
-      to_s
+    def to_hsl()
+      return "hsl(#{@hue}, #{@saturation}, #{@lightness})"
     end
 end
 
 svg.build do 
   cd = ColorDict.new
-  50.times do |i|
+  10.times do |i|
     cd.next_color
-    circle cx: rand(width), cy: rand(height), r: 100 + rand(100), fill: cd.to_s, stroke: cd.to_s, stroke_width: 2, stroke_opacity: 1
+    #stroke_opacity = [cd.opacity, cd.opacity + 0.4].min
+    circle(
+        cx: rand(width), 
+        cy: rand(height), 
+        r: circle_max_width/1.5 + rand(circle_max_width/3), 
+        fill: cd.to_hsla, 
+        fill_opacity: 0.6,
+        stroke: cd.to_hsl, 
+        stroke_width: 4, 
+        stroke_opacity: 0.8,
+    )
   end
 end
 
