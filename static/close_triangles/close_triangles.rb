610cd7a1a524c3f2fe36b40fd7e4a906e99044fa
karlwithak <nhrynuik@gmail.com>
2017-09-13 21:03:42 -0400

    Add some close trianges

--- static/close_triangles/close_triangles.rb
+++ static/close_triangles/close_triangles.rb
@@ -1,34 +1,40 @@
 require 'victor'
 
-svg = Victor::SVG.new width: 1000, height: 800, style: { background: '#fff' }
+svg = Victor::SVG.new width: 1000, height: 1000, style: { background: '#fff' }
 
-def next_color(color_dict, i, j, other) 
-  color_dict[:r] = (i * 25).to_i
-  color_dict[:b] = (j * 25).to_i
-  color_dict[:g] = ((i + j) * 2.8).to_i
-  return "rgb(#{color_dict[:r]}, #{color_dict[:g]}, #{color_dict[:b]})"
+class ColorDict
+    attr_accessor :opacity
+    def initialize()
+        @hue = 0
+        @saturation = "70%"
+        @lightness = "50%"
+    end
+
+    def next_color(j) 
+      @hue = ARGV[0].to_i + (j * 4)
+    end
+
+    def to_hsl()
+      return "hsl(#{@hue}, #{@saturation}, #{@lightness})"
+    end
 end
 
 svg.build do 
-  g transform: 'rotate(0 0 0)' do
-      ygap = 97
-      y = -112 
-      xgap = 0
-      color_dict = {r:0, g: 0, b: 0}
-      other = rand(256)
-      color_string = ''
-      10.times do |i|
-          y += ygap
-          x = -150 
-          x = -180 - ((i % 2) * 56)
-          11.times do |j|
-              #color_string = next_color(color_dict, i + 0.5, j + 0.5, other)
-              path d: "M #{x += (112 + xgap)},#{y} l -56,97 l 112,0 l -56,-97 Z", fill: color_string, stroke: color_string
-              color_string = next_color(color_dict, i, j, other)
-              path d: "M #{x += xgap},#{y} l 112,0 l -56,97 l -56,-97 Z", fill: color_string, stroke: color_string
-          end
+  ygap = 97
+  y = -112 
+  xgap = 0
+  color_dict = ColorDict.new
+  11.times do |i|
+      y += ygap
+      x = -150 
+      x = -180 - ((i % 2) * 56)
+      11.times do |j|
+          color_dict.next_color(j)
+          path d: "M #{x += (112 + xgap)},#{y} l -56,97 l 112,0 l -56,-97 Z", fill: color_dict.to_hsl, stroke: color_dict.to_hsl
+          color_dict.next_color(j + 0.5)
+          path d: "M #{x += xgap},#{y} l 112,0 l -56,97 l -56,-97 Z", fill: color_dict.to_hsl, stroke: color_dict.to_hsl
       end
-   end
+  end
 end
 
 svg.save 'close_triangles'
